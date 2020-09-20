4abbf257b8cf0803210d0b32baf8d29cd3949e13
Kai <m3sv.is.me@gmail.com>
2020-09-20 11:08:55 +1000

    Can register users to db

--- app.js
+++ app.js
@@ -7,8 +7,15 @@ bcrypt = require("bcryptjs");
 if(process.env.NODE_ENV !== "production") require("dotenv").config();
 const PORT = process.env.PORT;
 
+const User = require("./models/User");
+
 app.use(express.static(__dirname + "/public"));
-app.set("view engine", "ejs")
+app.use(express.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+
+mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true});
+
+// GET REQUESTS
 
 app.get("/", (req, res) => {
     res.render("index", {title: "Home"});
@@ -20,6 +27,28 @@ app.get("/register", (req, res) => {
 
 app.get("/login", (req, res) => {
     res.render("login", {title: "Login"});
+});
+
+// POST REQUESTS
+app.post("/register", async (req, res) => {
+    let hash = bcrypt.hashSync(req.body.user.password, 14);
+    req.body.user.password = hash;
+    let user = new User(req.body.user);
+    await user.save((err) => {
+        if(err) {
+            console.log("something went wrong:\n" + err);
+            res.redirect("/");
+        }
+        else res.redirect("/login");
+    });
+    // check if email is already in use
+    // User.find({email: req.body.user.email}, (err, users) => {
+    //     if(err) {
+    //         console.log(err);
+    //         return res.redirect("/");
+    //     }
+    //     console.log(users);
+    // });
 });
 
 app.listen(PORT, console.log(`Serving port ${PORT} at http://localhost:${PORT}`));
