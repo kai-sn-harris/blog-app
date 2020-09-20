237ee7fa469f0ea84ef04e9187ae4816e0d82d10
Kai <m3sv.is.me@gmail.com>
2020-09-20 21:06:18 +1000

    make sure username is unique

--- models/User.js
+++ models/User.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose"),
 uniqueValidator = require("mongoose-unique-validator");
 
 const userSchema = new mongoose.Schema({
-    username: {type: String, required: true},
+    username: {type: String, required: true, unique: true},
     email: {type: String, required: true, unique: true},
     password: {type: String, required: true},
     posts: [{type: mongoose.Schema.Types.ObjectId, ref: "post"}]
