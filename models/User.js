83faba0a399dd4e08d07249da4b0cf2824b64192
Kai <m3sv.is.me@gmail.com>
2020-09-20 14:37:41 +1000

    Removed unnecessary tag for email

--- models/User.js
+++ models/User.js
@@ -3,7 +3,7 @@ uniqueValidator = require("mongoose-unique-validator");
 
 const userSchema = new mongoose.Schema({
     username: {type: String, required: true},
-    email: {type: String, required: true, unique: true, dropDups: true},
+    email: {type: String, required: true, unique: true},
     password: {type: String, required: true}
 });
 
