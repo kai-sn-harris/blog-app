c726a22210370824de6bffe0f0197eb6aa7e54d7
Kai <m3sv.is.me@gmail.com>
2020-09-27 11:17:25 +1000

    Resets bio, username, checkbox to db values if user doesnt update profile

--- public/profile.js
+++ public/profile.js
@@ -1,9 +1,20 @@
 const editBtn = document.querySelector(".edit-btn"),
-display = document.querySelector(".display"),
-edit = document.querySelector(".edit");
+display       = document.querySelector(".display"),
+edit          = document.querySelector(".edit");
+
+let displayMode = true;
 
 editBtn.addEventListener("click", event => {
     display.classList.toggle("hidden");
     edit.classList.toggle("hidden");
-    editBtn.textContent === "Edit" ? editBtn.textContent = "Display" : editBtn.textContent = "Edit";
+    displayMode = !displayMode;
+    // We are in edit mode (says display because if we click the button we go to display mode)
+    if(!displayMode) {
+        document.querySelector("input#username").value = username;
+        document.querySelector("textarea#bio").value = bio;
+        document.querySelector("input#private").checked = private;
+        console.log(bio)
+    }
+    !displayMode ? editBtn.textContent = "Back" : editBtn.textContent = "Edit";
 });
+
