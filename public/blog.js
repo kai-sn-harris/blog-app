faf383ba13af6c1b2cf4f3792f87892e640b27b9
Kai <m3sv.is.me@gmail.com>
2020-10-30 18:09:12 +1100

    removed scroll bar and in turn redundant code

--- public/blog.js
+++ public/blog.js
@@ -2,21 +2,6 @@ const forms = document.querySelectorAll(".del-form"),
 yesBtn = document.querySelector("#yes-btn"),
 noBtn = document.querySelector("#no-btn");
 
-function disableScroll() { 
-    // Get the current page scroll position 
-    scrollTop = window.pageYOffset || document.documentElement.scrollTop; 
-    scrollLeft = window.pageXOffset || document.documentElement.scrollLeft, 
-  
-    // if any scroll is attempted, set this to the previous value 
-    window.onscroll = function() { 
-        window.scrollTo(scrollLeft, scrollTop); 
-    }; 
-} 
-  
-function enableScroll() { 
-    window.onscroll = function() {}; 
-} 
-
 function openModal() {
     let modal = document.querySelector("#modal");
     modal.style.display === "none" ? modal.style.display = "flex" : modal.style.display = "none";
@@ -28,7 +13,8 @@ forms.forEach(form => {
     form.addEventListener("submit", event => {
         toSubmit = form;
         event.preventDefault();
-        disableScroll();
+        // remove scroll bar from body (also prevents scrolling)
+        document.querySelector("body").style.overflow = "hidden";
         openModal();
     });
 });
@@ -38,7 +24,8 @@ yesBtn.addEventListener("click", () => {
 });
 
 noBtn.addEventListener("click", () => {
-    enableScroll();
     modal.style.display = "none";
+    // Add scroll back
+    document.querySelector("body").style.overflow = "scroll";
     toSubmit = null;
 });
